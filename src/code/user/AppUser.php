9088d7c3d3942c8e651d9ab35e0152574435f1e4
stefano.cavazzini <digiworkscom@gmail.com>
2021-11-19 14:38:28 +0100

    Update AppUser.php

--- src/code/user/AppUser.php
+++ src/code/user/AppUser.php
@@ -4,7 +4,18 @@ namespace code\user;
 
 abstract class AppUser {
     
+    public abstract function getHash() : string;
+    
     public function passwordHash(string $value){
         return password_hash($value,PASSWORD_DEFAULT);
+    }
+    
+    public function verifyPassword(string $value){
+        $ret = false;
+        $pwdHash = $this->passwordHash($value);
+        if($pwdHash === $this->getHash()){
+            $ret = true;
+        }
+        return $ret;
     }
 }
