ff8a66924769524d0dff1bdaeb9001662ba1fb7c
stefano.cavazzini <digiworkscom@gmail.com>
2021-11-29 14:38:25 +0100

    Update Loader.php

--- src/code/renders/Loader.php
+++ src/code/renders/Loader.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace code\renders;
 
 use code\applications\ApiAppFactory;
@@ -6,60 +7,48 @@ use code\exceptions\ServerScriptDoesNotExist;
 use code\service\ServiceTypes;
 use code\utility\Curl;
 
+abstract class Loader {
 
-abstract class Loader 
-{
     private $parts = [];
-    
-    
-    public function addPart($viewPart)
-    {
+
+    public function addPart($viewPart) {
         $this->parts[] = $viewPart;
         return $this;
     }
-    
-    public function load()
-    {
+
+    public function load() {
         $fileSystem = ApiAppFactory::getApp()->getService(ServiceTypes::FILESYSTEM);
         $view = "";
-        foreach($this->parts as $part)
-        {
+        foreach ($this->parts as $part) {
             if (!$fileSystem->fileExists($part)) {
-               throw ServerScriptDoesNotExist::atPath($part);
+                throw ServerScriptDoesNotExist::atPath($part);
             }
             $script = Curl::get($part);
-            $view .= $script ."\n";
+            $view .= $script . "\n";
         }
         return $view;
     }
-    
+
     /**
      * 
      * @param string $script
      * @return string
      */
-    protected function compress($script)
-    {
-       $script = str_replace("'", "\x27", $script);
-       //$this->sinlgeLineComments($script);
-       $script = trim(preg_replace('/[\t\n\r\s]+/', ' ', $script));
-       $script = trim(preg_replace( '/>(\s)+</m', '><', $script));
-       return $script;
+    protected function compress($script) {
+        $script = str_replace("'", "\x27", $script);
+        $this->removeComments($script);
+        $script = trim(preg_replace('/[\t\n\r\s]+/', ' ', $script)); // tabs,newlines,etc.
+        $script = trim(preg_replace('/>(\s)+</m', '><', $script)); // remove spaces
+        return $script;
     }
-    
+
     /**
      * 
      * @param string $output
      */
-    protected function sinlgeLineComments(&$script) 
-    {
-        $script = preg_replace_callback('#^((?:(?!/\*).)*?)//(.*)#m',
-        create_function(
-          '$match',
-          'return "/* " . trim(mb_substr($match[1], 0)) . " */";'
-        ), $script
-       );
+    protected function removeComments(&$script) {
+        $script = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $script); // remove comments
     }
-    
+
     public abstract function render();
 }
