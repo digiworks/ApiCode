7e7891cd74acaae9274fb01e21d5a66bf2c0256c
stefano.cavazzini <digiworkscom@gmail.com>
2021-12-10 10:35:55 +0100

    Update ResponseBuilder.php

--- src/code/controllers/ResponseBuilder.php
+++ src/code/controllers/ResponseBuilder.php
@@ -78,7 +78,7 @@ class ResponseBuilder {
         'connect-src' => "'self'",
         'img-src' => "'self'",
         'script-src' => "'self'",
-        'style-src' => "'self'"
+        'style-src' => "'unsafe-inline'"//"'self'"
     ];
 
     /**
