f9725b4f4fb4ba8ef27f7ffd1fd4f6dbb54a8ab2
stefano.cavazzini <digiworkscom@gmail.com>
2022-05-03 11:05:06 +0200

    Update LocalFS.php

--- src/code/storage/filesystem/drivers/LocalFS.php
+++ src/code/storage/filesystem/drivers/LocalFS.php
@@ -161,7 +161,7 @@ class LocalFS implements StorageDriverInterface {
      * @return type
      */
     public function fileExists(string $path): bool {
-        return is_file($this->basePath . DIRECTORY_SEPARATOR . $path);
+        return is_file($path);
     }
 
     /**
@@ -170,7 +170,7 @@ class LocalFS implements StorageDriverInterface {
      * @return type
      */
     public function dirname($url) {
-        return str_replace($this->basePath . DIRECTORY_SEPARATOR, '', dirname($url));
+        return dirname($url);
     }
 
     /**
