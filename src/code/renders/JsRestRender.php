4f8a2e634241b1be04fc25ef627357625b53945c
stefano.cavazzini <digiworkscom@gmail.com>
2022-05-27 09:24:18 +0200

    Update JsRestRender.php

--- src/code/renders/JsRestRender.php
+++ src/code/renders/JsRestRender.php
@@ -2,9 +2,20 @@
 
 namespace code\renders;
 
+use code\applications\ApiAppFactory;
+use code\configuration\Configurations;
+use code\service\ServiceTypes;
 use code\utility\Arr;
 
 class JsRestRender extends JsRender {
+
+    private $appVersion = '';
+
+    public function __construct($conf) {
+        parent::__construct($conf);
+        $env = ApiAppFactory::getApp()->getService(ServiceTypes::CONFIGURATIONS)->get(Configurations::ENV);
+        $this->appVersion = $env['version'];
+    }
 
     /**
      * 
@@ -30,14 +41,14 @@ class JsRestRender extends JsRender {
         $jsString = " baseApp.indexPageApiGateway = '" . $this->controller->getFullUrl() . "';";
         return $jsString . " ";
     }
-    
+
     /**
      * 
      * @param string $import
      * @return $this
      */
     public function addImport(string $import) {
-        $import['lib'] = $this->controller->getFullUrl() . "/api/file/js/" . $import['lib'];
+        $import['lib'] = $this->controller->getFullUrl() . "/api/file/js/" . $import['lib'] . '?ver=' . $this->appVersion;
         $this->imports[] = $import;
         return $this;
     }
@@ -50,7 +61,7 @@ class JsRestRender extends JsRender {
     public function addImports(array $imports) {
         $chg_imports = [];
         foreach ($imports as $import) {
-            $import['lib'] = $this->controller->getFullUrl() . "/api/file/js/" . $import['lib'];
+            $import['lib'] = $this->controller->getFullUrl() . "/api/file/js/" . $import['lib'] . '?ver=' . $this->appVersion;
             $chg_imports[] = $import;
         }
         $this->imports = Arr::mergeRecursive($this->imports, $chg_imports);
